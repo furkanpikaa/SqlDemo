ea4783a6b2b00fe2e7019a5c3213cb1df13c93e1
Furkan <116648662+furkanpikaa@users.noreply.github.com>
2023-07-19 21:02:55 +0300

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,25 @@
+﻿--Select
+--ANSII
+SELECT ContactName Adi,CompanyName SirketAdi,City Sehir from Customers
+
+Select * from Customers where City = 'London'
+
+--Case insensitive  => buyuk, kucuk harf kullaniminda 
+
+Select * from Products where CategoryID = 1 or CategoryID = 3
+
+Select * from Products where CategoryID = 1 and UnitPrice >= 10   --where => nerede
+
+select * from Products order by ProductName  --order by => sirala
+
+select * from Products order by UnitPrice desc   --ascending artan  --descending dusen
+
+select count(*) from Products --tüm satirlari say
+
+select count(*) Adet from Products where CategoryID = 2       --kategoriId`si 2 olanlari say
+
+select categoryId,count(*) Adet from Products group by CategoryID    
+
+select categoryId,count(*) Adet from Products group by CategoryID having count(*) < 10
+
+select categoryId,count(*) Adet from Products where UnitPrice > 20 group by CategoryID having count(*) < 10
