7a761ea5728f0fe100997951cefe70d64fc6edc2
Furkan <116648662+furkanpikaa@users.noreply.github.com>
2023-07-24 13:21:59 +0300

    Add files via upload

---
+++ SQLQueryOdev.sql
@@ -0,0 +1,6 @@
+﻿SELECT Products.ProductName aS 'urun adi' , SUM([Order Details].UnitPrice * [Order Details].Quantity) as 'Kazanilan toplam miktar'
+from Products 
+inner join [Order Details] on Products.ProductID = [Order Details].ProductID
+inner join Orders on [Order Details].OrderID = Orders.OrderID
+group by Products.ProductName
+order by [Kazanilan toplam miktar]
